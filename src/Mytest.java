87c534e7bf50b23a44808c711cde5ba2507e3649
Evgeniy <ablaevgeniy@mail.ru>
2022-10-05 15:42:16 +0600

    feat: add additional console log

--- src/Mytest.java
+++ src/Mytest.java
@@ -1,5 +1,7 @@
 public class Mytest {
     public static void main(String[] args) {
         System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
     }
 }
